1f30d8c7e9d14f8ed54276c6e8e68b48d4d423f8
Viraj Jadhav <virajssjadhav@gmail.com>
2020-10-18 02:26:27 +0530

    add: begin with POST method, added body parsing in POST

--- http_server.py
+++ http_server.py
@@ -8,6 +8,12 @@ filePath = "/home/viraj007/Semester 5/CN/Project/final/ResponseFiles/"
 STATUSCODE = None
 imageFileExtensions = [".png", ".jpeg", ".jpg",
                        ".ico", ".webp", ".apng", ".gif", ".bmp", ".svg"]
+Response = {
+    "Date": "",
+    "Server": "Delta-Server/0.0.1 (Ubuntu)",
+    "Connection": "Close",
+    # "Status": STATUSCODE str()
+}
 
 
 def switchStatusCode(code=None):
@@ -43,11 +49,14 @@ def httpDateFormat():
     return str(formatdate(timeval=None, localtime=False, usegmt=True))
 
 
-def getParsedData(connectionData=[]):
+def getParsedData(connectionData=None):
     parsedData = connectionData.split("\r\n")
+    # print(parsedData)
+    headerEndCount = 0
     requestedMethod = None
     requestedPath = ""
     httpVersion = None
+    requestBody = {}
     restHeaders = dict()
     firstLine = parsedData[0].split()
     if len(firstLine) == 3:
@@ -58,12 +67,41 @@ def getParsedData(connectionData=[]):
     elif len(firstLine) == 1:
         requestedMethod = firstLine[0]
     for data in parsedData[1:]:
+        headerEndCount += 1
         try:
             key, value = data.split(":", 1)
             restHeaders[key] = value
         except:
-            pass
-    return requestedMethod, requestedPath, httpVersion, restHeaders
+            break
+    if requestedMethod == "GET":
+        pass
+    elif requestedMethod == "POST":
+        if "application/x-www-form-urlencoded" in restHeaders["Content-Type"]:
+            tempBody = parsedData[headerEndCount + 1].split("&")
+            for tbody in tempBody:
+                try:
+                    key, value = tbody.split("=")
+                    tvalue = value.split("%")
+                    value = tvalue[0]
+                    if len(tvalue) > 1:
+                        for i in range(1, len(tvalue)):
+                            try:
+                                bytesData = bytes.fromhex(
+                                    tvalue[i][:2])
+                                value += bytesData.decode("ASCII") + \
+                                    tvalue[i][2:]
+                            except:
+                                pass
+                    requestBody[key] = value
+                except:
+                    pass
+        elif "multipart/form-data" in restHeaders["Content-Type"]:
+            # reqBoundary = restHeaders["Content-Type"].split("boundary=")[1]
+            reqBody = parsedData[headerEndCount + 1:]
+            for i in range(1, len(reqBody) - 4, 4):
+                tkey = reqBody[i].split(";")[1].split("name=")[1].strip("\"")
+                requestBody[tkey] = reqBody[i + 2]
+    return requestedMethod, requestedPath, httpVersion, restHeaders, requestBody
 
 
 def getValidFilePath(requestedPath=""):
@@ -71,11 +109,6 @@ def getValidFilePath(requestedPath=""):
     if requestedPath == None:
         STATUSCODE = 404
         return filePath + "not_found.html"
-    # path = requestedPath.split("/")
-    # if path[1] == '':
-    #     return filePath + "index.html"
-    # else:
-    #     return filePath + "/".join(path[1:])
     if requestedPath == "/":
         return filePath + "index.html"
     else:
@@ -108,62 +141,63 @@ def getRequestedFile(requestedPath="", fileMode=""):
 
 
 def handleGETRequest(httpVersion="", restHeaders={}, requestedPath=""):
-    global STATUSCODE, imageFileExtensions, filePath
+    global STATUSCODE, imageFileExtensions, filePath, Response
     finalFile = response = ""
     fileExtension = "html"
     STATUSCODE = 200
-    Response = {
-        "Date": httpDateFormat(),
-        "Server": "Delta-Server/0.0.1 (Ubuntu)",
-        "Connection": "Close",
-        # "Status": STATUSCODE str()
-    }
+    Response["Date"] = httpDateFormat()
     if httpVersion != "HTTP/1.1" or "Host" not in restHeaders:
         STATUSCODE = 400
         httpVersion = "HTTP/1.1"
         with open(filePath + "bad_request.html", "r") as requestedFile:
             finalFile = requestedFile.read()
-        response = httpVersion + switchStatusCode(STATUSCODE) + "\n"
+        response = httpVersion + switchStatusCode(STATUSCODE) + "\r\n"
         Response["Content-Type"] = switchContentType(fileExtension)
         Response["Content-Length"] = str(len(finalFile))
         # Response["Status"] = STATUSCODE
         for key, value in Response.items():
-            response += key + ": " + value + "\n"
-        response += "\n" + finalFile + "\n"
+            response += key + ": " + value + "\r\n"
+        response += "\r\n" + finalFile + "\n"
         response = response.encode()
     elif requestedPath.endswith((".html", "/", ".json", ".js")):
         finalFile, fileExtension = getRequestedFile(requestedPath, "r")
-        response = httpVersion + switchStatusCode(STATUSCODE) + "\n"
+        response = httpVersion + switchStatusCode(STATUSCODE) + "\r\n"
         Response["Content-Type"] = switchContentType(fileExtension)
         Response["Content-Length"] = str(len(finalFile))
         # Response["Status"] = STATUSCODE
         for key, value in Response.items():
-            response += key + ": " + value + "\n"
-        response += "\n" + finalFile + "\n"
+            response += key + ": " + value + "\r\n"
+        response += "\r\n" + finalFile + "\n"
         response = response.encode()
     elif requestedPath.endswith(tuple(imageFileExtensions)):
         finalFile, fileExtension = getRequestedFile(requestedPath, "rb")
-        response = httpVersion + switchStatusCode(STATUSCODE) + "\n"
+        response = httpVersion + switchStatusCode(STATUSCODE) + "\r\n"
         Response["Content-Type"] = switchContentType(fileExtension)
         Response["Content-Length"] = str(len(finalFile))
         # Response["Status"] = STATUSCODE
         for key, value in Response.items():
-            response += key + ": " + value + "\n"
-        response += "\n"
+            response += key + ": " + value + "\r\n"
+        response += "\r\n"
         response = response.encode() + finalFile
     else:
         STATUSCODE = 404
         with open(filePath + "not_found.html", "r") as requestedFile:
             finalFile = requestedFile.read()
-        response = httpVersion + switchStatusCode(STATUSCODE) + "\n"
+        response = httpVersion + switchStatusCode(STATUSCODE) + "\r\n"
         Response["Content-Type"] = switchContentType(fileExtension)
         Response["Content-Length"] = str(len(finalFile))
         # Response["Status"] = STATUSCODE
         for key, value in Response.items():
-            response += key + ": " + value + "\n"
-        response += "\n" + finalFile + "\n"
+            response += key + ": " + value + "\r\n"
+        response += "\r\n" + finalFile + "\n"
         response = response.encode()
     return response
+
+
+def handlePOSTRequest(httpVersion="", restHeaders={}, requestedPath="", requestBody={}):
+    global Response
+    Response["Date"] = httpDateFormat()
+    pass
 
 
 def eachClientThread(clientConnection=None):
@@ -172,19 +206,22 @@ def eachClientThread(clientConnection=None):
     while True:
         try:
             connectionData = clientConnection.recv(1024).decode('utf-8')
-            requestedMethod, requestedPath, httpVersion, restHeaders = getParsedData(
+            # print(connectionData)
+            requestedMethod, requestedPath, httpVersion, restHeaders, requestBody = getParsedData(
                 connectionData)
+            response = b'HTTP/1.1 200 OK\r\n'
             if requestedMethod == "GET":
                 response = handleGETRequest(
                     httpVersion, restHeaders, requestedPath)
             elif requestedMethod == "POST":
-                pass
-            elif requestedMethod == "PUT":
-                pass
-            elif requestedMethod == "DELETE":
-                pass
-            elif requestedMethod == "HEAD":
-                pass
+                response = handlePOSTRequest(
+                    httpVersion, restHeaders, requestedPath, requestBody)
+            # elif requestedMethod == "PUT":
+            #     pass
+            # elif requestedMethod == "DELETE":
+            #     pass
+            # elif requestedMethod == "HEAD":
+            #     pass
             clientConnection.send(response)
             # try:
             #     totalClientConnections.remove(clientConnection)
